543bbebb60bf853292f59a9e7b6b23cfb58df75d
alenior <alenior@gmail.com>
2024-05-15 18:50:24 -0300

    Update des100

--- apr001/des100.py
+++ apr001/des100.py
@@ -1,19 +1,25 @@
 from random import randint
+from time import sleep
 
 
-def sorteia(pri, ult, qtd):
-    sorteados = []
-    while qtd > 0:
-        sorteados.append(randint(pri, ult))
-        qtd -= 1
-    print(f'{sorteados}')
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
 
 
-def somaPar():
+def somapar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista} temos {soma}.')
 
 
-
-pri = int(input('Informe o primeiro número da sequencia para sorteio: '))
-ult = int(input('Informe o último número da sequencia para sorteio: '))
-qtd = int(input('Informe a quantidade de números a ser sorteados: '))
-sorteia(pri, ult, qtd)
+números = list()
+sorteia(números)
+somapar(números)
